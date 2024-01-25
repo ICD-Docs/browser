25ca2e2dcf5aea87548141e07ca970da7f4bb56b
Marc Donada <marc.donada@outlook.com>
2024-01-25 10:03:00 +0100

    merged all languages docker scripts in one script

--- local-live-edit.ps1
+++ local-live-edit.ps1
@@ -1,10 +1,15 @@
+${lang} = "en" # select a language: ar en es fr ru tr zh
+
+Write-Output "current language: ${lang}"
+
 # RUN THE docs for live editing. Run to create the container
 docker run -ti `
-           --name browser-ar-mkdocs `
+           --name browser-${lang}-mkdocs `
            -p 8080:8000 `
-           -v C:/code/GitRepos/Docs/browser/ar:/mkdocs `
+           -v C:/code/GitRepos/Docs/browser/${lang}:/mkdocs `
            -v C:/code/gitrepos/DocsBuilder/docbuilder/custom_readthedocs:/mkdocs/custom_readthedocs `
            --env LIVE_RELOAD_SUPPORT=true `
            polinux/mkdocs 
+
 # If it is already created, make sure it's running
-docker start browser-ar-mkdocs
+docker start browser-${lang}-mkdocs
